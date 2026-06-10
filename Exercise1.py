184362984a84b2e6ca2ffb2e5eb50adb206f79d4
Bhosale Veerashree <bhosale.24bce10378@vitbhopal.ac.in>
2026-06-09 20:10:23 -0700

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,13 @@
+#Rectangle area calculation
+length = input("Enter the length of the rectangle: ")
+width = input("Enter the width of the rectangle: ")
+Area = int(length) * int(width)
+
+print(Area)
+
+length1 = input("Enter the length of the rectangle: ")
+width1 = input("Enter the width of the rectangle: ")
+
+Area1 = float(length1) * float(width1)
+
+print(Area1)
